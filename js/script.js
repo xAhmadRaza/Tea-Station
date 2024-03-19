67802c0b81a4d76a8c50d495e23824e618925cdb
xAhmadRaza <Gintoki.sakata789@gmail.com>
2024-03-20 03:37:00 +0500

    feat: add navigation bar and pages

--- js/script.js
+++ js/script.js
@@ -1,3 +1,15 @@
 const copyrightEL = document.querySelector("#copyright__date");
 // copyright date setup
 copyrightEL.textContent = new Date().getFullYear();
+// setup nav
+const navBtn = document.querySelector(".nav__btn");
+const navbar = document.querySelector("#navbar");
+const navClose = document.querySelector("#nav__close");
+// show nav
+navBtn.addEventListener("click", () => {
+  navbar.classList.add("show__nav");
+});
+// close nav
+navClose.addEventListener("click", () => {
+  navbar.classList.remove("show__nav");
+});
